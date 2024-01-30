b941b8dce580561913d29e6f7f3d2eb6783c262d
ishowta <ishowta@gmail.com>
2024-01-31 02:18:58 +0900

    fix: change Array sort to List sort to fix all

--- src/analyser.rs
+++ src/analyser.rs
@@ -1,16 +1,15 @@
-use std::cell::RefCell;
+use std::{cell::RefCell, io::Write, process::Command};
 
 use log::debug;
 use miette::{Diagnostic, SourceSpan};
+use tempfile::NamedTempFile;
 use thiserror::Error;
-use z3::{Config, Context as Z3Context, SatResult, Solver};
 
 use crate::{
     ast::{Ast, Expression, Node, Rule, RuleGroup},
     symbol::{Bindings, SymbolReferences},
 };
 
-// TODO: use z3 wrapper
 type Symbol = String;
 type Declaration = String;
 type Constraint = String;
@@ -52,14 +51,13 @@ struct AnalysysContext<'a, 'ctx> {
     pub bindings: &'a Bindings<'a>,
     pub symbol_references: &'a SymbolReferences<'a>,
     pub source_code: &'a String,
-    pub z3_context: &'ctx Z3Context,
     pub declarations: &'ctx RefCell<Vec<Declaration>>,
 }
 
 fn check_expression(ctx: &AnalysysContext, rule: &Expression) -> ExpressionProperties {
     ctx.declarations.borrow_mut().push(
         r#"
-(declare-const keys (Array String Bool))
+(declare-const keys (Seq String))
 
 (declare-const arr (Seq String))
 
@@ -67,7 +65,7 @@ fn check_expression(ctx: &AnalysysContext, rule: &Expression) -> ExpressionPrope
 
 (declare-const literal_3 (Int))
 
-(declare-const bar_inner String)
+(declare-const bar_inner Int)
 (declare-const bar Refl)
 "#
         .to_owned(),
@@ -76,25 +74,65 @@ fn check_expression(ctx: &AnalysysContext, rule: &Expression) -> ExpressionPrope
         symbol: "".to_owned(),
         constraints: vec![
             // keys = data.keys()
-            format!("(forall ((key String)) (= (not (= (select request_resource_data_inner key) undefined)) (= (select keys key) true)))"),
+            format!("(= keys (list-keys request_resource_data_inner))"),
+            //format!("(forall ((key String)) (= (not (= (list-get request_resource_data_inner key) undefined)) (= (select keys key) true)))"),
             // arr = ['foo', 'baz']
-            format!("(= arr (seq.++ (seq.unit \"foo\") (seq.unit \"bar\")))"),
+            format!("(= arr (seq.++ (seq.unit \"foo\") (seq.unit \"bar\") (seq.unit \"hoge\")))"),
             // keys.hasOnly(arr)
-            format!("(forall ((key String)) (implies (= (select keys key) true) (seq.contains arr (seq.unit key))))"),
-            format!("(= arr request_resource_data_shape)"),
+            //format!("(forall ((key String)) (implies (= (select keys key) true) (seq.contains arr (seq.unit key))))"),
+            format!("(= arr keys)"),
             // 'foo' in data
-            format!("(= (select request_resource_data_inner \"foo\") foo)"),
+            format!("(= (list-get request_resource_data_inner \"foo\") foo)"),
             // 3
             format!("(= literal_3 3)"),
             // foo == 3
             format!("(= foo (int literal_3))"),
             // 'bar' in data
-            format!("(= (select request_resource_data_inner \"bar\") bar)"),
-            // bar.len() < n
-            format!("(< (str.len bar_inner) 1000)"),
+            format!("(= (list-get request_resource_data_inner \"bar\") bar)"),
+            // bar is string
             format!("(= bar (str bar_inner))"),
+            // bar.len() < n
+            format!("(< bar_inner 100)"),
         ],
         errors: vec![],
+    }
+}
+
+fn run_z3(source: &String) -> String {
+    let mut source_file = NamedTempFile::new().unwrap();
+    let _ = source_file.write_all(source.as_bytes());
+    let command_result = Command::new("z3").arg(source_file.path()).output();
+    let command_output = String::from_utf8_lossy(&command_result.unwrap().stdout)
+        .trim()
+        .into();
+    command_output
+}
+
+#[derive(Clone, Debug, PartialEq, Eq)]
+enum SolverResult {
+    Sat(String),
+    Unsat,
+    Unknown,
+}
+
+fn solve(source: &String) -> SolverResult {
+    let input = format!(
+        "
+{}
+
+(check-sat)
+(get-model)
+",
+        source
+    );
+    let output = run_z3(&input);
+    match output.split("\n").nth(0) {
+        Some("sat") => {
+            SolverResult::Sat(output.split("\n").skip(1).collect::<Vec<&str>>().join("\n"))
+        }
+        Some("unsat") => SolverResult::Unsat,
+        Some("unknown") => SolverResult::Unknown,
+        _ => panic!(),
     }
 }
 
@@ -104,14 +142,14 @@ fn check_rule(ctx: &AnalysysGlobalContext, rule: &Rule) -> Vec<AnalysysError> {
         rule.get_span().0.start_point.row + 1
     );
 
-    let config = Config::default();
     let ctx = AnalysysContext {
         bindings: ctx.bindings,
         symbol_references: ctx.symbol_references,
         source_code: ctx.source_code,
-        z3_context: &Z3Context::new(&config),
         declarations: &RefCell::new(vec![format!(
             r#"
+(declare-datatypes (T1 T2) ((Entry (entry (key T1) (value T2)))))
+
 (declare-datatypes ((Refl 0)) (
     (
         (undefined)
@@ -119,13 +157,13 @@ fn check_rule(ctx: &AnalysysGlobalContext, rule: &Rule) -> Vec<AnalysysError> {
         ;(bool (bool_val Bool))
         (int (int_val Int))
         ;(float (float_val Float64))
-        (str (str_val String))
+        (str (str_bytes Int))
         ;(char (char_val Unicode))
         ;(duration)
         ;(latlng)
         ;(timestamp)
         ;(list (list_val (Seq Refl)))
-        (map (map_val (Array String Refl)) (map_shape (Seq String)))
+        (map (map_val (List (Entry String Refl))))
         ;(mapdiff (mapdiff_left (Array String Refl)) (mapdiff_right (Array String Refl)))
         ;(path (path_val String))
         ;(pathubt (pathubt_val (Seq String)))
@@ -133,23 +171,53 @@ fn check_rule(ctx: &AnalysysGlobalContext, rule: &Rule) -> Vec<AnalysysError> {
     )
 ))
 
-(declare-const request_resource_data Refl)
-(declare-const request_resource_data_inner (Array String Refl))
-(declare-const request_resource_data_shape (Seq String))
-;(assert (forall ((key String)) (= (not (= (select request_resource_data_inner key) undefined)) (seq.contains request_resource_data_shape (seq.unit key)))))
-(assert (= request_resource_data (map request_resource_data_inner request_resource_data_shape)))
+(define-fun-rec
+    list-get
+    (
+        (lst (List (Entry String Refl)))
+        (sk String)
+    )
+    Refl
+    (if
+        (= lst nil)
+        undefined
+        (if
+            (= (key (head lst)) sk)
+            (value (head lst))
+            (list-get (tail lst) sk)
+        )
+    )
+)
 
-(declare-const request_resource Refl)
-(declare-const request_resource_inner (Array String Refl))
-(declare-const request_resource_shape (Seq String))
-(assert (= (select request_resource_inner "data") request_resource_data))
-(assert (= request_resource (map request_resource_inner request_resource_shape)))
+(define-fun-rec
+    list-keys
+    (
+        (lst (List (Entry String Refl)))
+    )
+    (Seq String)
+    (if
+        (= lst nil)
+        (as seq.empty (Seq String))
+        (seq.++
+            (seq.unit (key (head lst)))
+            (list-keys (tail lst))
+        )
+    )
+)
 
-(declare-const request Refl)
-(declare-const request_inner (Array String Refl))
-(declare-const request_shape (Seq String))
-(assert (= (select request_inner "resource") request_resource))
-(assert (= request (map request_inner request_shape)))
+;(declare-const request_resource_data Refl)
+(declare-const request_resource_data_inner (List (Entry String Refl)))
+;(assert (= request_resource_data (map request_resource_data_inner)))
+
+;(declare-const request_resource Refl)
+;(declare-const request_resource_inner (List (Entry String Refl)))
+;(assert (= (list-get request_resource_inner "data") request_resource_data))
+;(assert (= request_resource (map request_resource_inner)))
+
+;(declare-const request Refl)
+;(declare-const request_inner (List (Entry String Refl)))
+;(assert (= (list-get request_inner "resource") request_resource))
+;(assert (= request (map request_inner)))
 "#
         )]),
     };
@@ -166,7 +234,6 @@ fn check_rule(ctx: &AnalysysGlobalContext, rule: &Rule) -> Vec<AnalysysError> {
         let constraint = constraints.iter().fold("".to_owned(), |acc, constraint| {
             format!("{}\n(assert {})", acc, constraint)
         });
-        let solver = Solver::new(&ctx.z3_context);
         let source_code = format!(
             "
 {}
@@ -176,15 +243,13 @@ fn check_rule(ctx: &AnalysysGlobalContext, rule: &Rule) -> Vec<AnalysysError> {
             constraint
         );
         debug!("source code:\n{}", source_code);
-        solver.from_string(source_code);
-        match solver.check() {
-            SatResult::Sat => {
+        match solve(&source_code) {
+            SolverResult::Sat(example) => {
                 debug!("sat");
-                let model = solver.get_model().unwrap();
-                debug!("truthly example:\n{}", model);
+                debug!("truthly example:\n{}", example);
             }
-            SatResult::Unsat => errors.push(AnalysysError::new(format!("Always false"), rule)),
-            SatResult::Unknown => errors.push(AnalysysError::new(
+            SolverResult::Unsat => errors.push(AnalysysError::new(format!("Always false"), rule)),
+            SolverResult::Unknown => errors.push(AnalysysError::new(
                 format!("Static analysis failed because this conditions are too complex."),
                 rule,
             )),
@@ -229,82 +294,33 @@ fn check_rule(ctx: &AnalysysGlobalContext, rule: &Rule) -> Vec<AnalysysError> {
     {
         let limit_constraint = r#"
 ; 1MB limit
-(declare-const sizes (Array String Int))
-;(assert (forall ((key String) (value Int)) (implies (= (select request_resource_data_inner key) (int value)) (= (select sizes key) 8))))
-;(assert (forall ((key String)) (implies (= (select request_resource_data_inner key) undefined) (= (select sizes key) 0))))
-(assert
-    (=
-        true
-        (seq.foldl
-            (lambda
-                ((acc Bool) (key String))
-                (and
-                    acc
-                    (forall
-                        ((value Int))
-                        (implies
-                            (=
-                                (select request_resource_data_inner key)
-                                (int value)
-                            )
-                            (=
-                                (select sizes key)
-                                8
-                            )
-                        )
-                    )
-                    (forall
-                        ((value String))
-                        (implies
-                            (=
-                                (select request_resource_data_inner key)
-                                (str value)
-                            )
-                            (=
-                                (select sizes key)
-                                (str.len value)
-                            )
-                        )
-                    )
-                    (forall
-                        ((value (Array String Refl)) (shape (Seq String)))
-                        (implies
-                            (=
-                                (select request_resource_data_inner key)
-                                (map value shape)
-                            )
-                            (=
-                                (select sizes key)
-                                0
-                            )
-                        )
-                    )
-                    (implies
-                        (=
-                            (select request_resource_data_inner key)
-                            undefined
-                        )
-                        (=
-                            (select sizes key)
-                            0
-                        )
-                    )
-                )
-            )
-            true
-            arr
+
+(define-fun-rec
+    list-sum
+    (
+        (lst (List (Entry String Refl)))
+    )
+    Int
+    (if
+        (= lst nil)
+        0
+        (+
+            (match (value (head lst)) (
+                ((int x) 8)
+                ((str x) x)
+                (undefined 0)
+                ((map x) (list-sum x))
+            ))
+            (list-sum (tail lst))
         )
     )
 )
-;(declare-const size_list (Seq Int))
-;(assert (forall ((key String)) (implies (not (= (select sizes key) 0)) (not (= (seq.indexof size_list (seq.unit (select sizes key))) -1)))))
-(assert (> (seq.foldl (lambda ((acc Int) (key String)) (+ acc (select sizes key))) 0 arr) 262144))
+(assert (> (list-sum request_resource_data_inner) 1000))
         "#;
         debug!("check 1MB limit");
         let constraint = constraints.iter().fold("".to_owned(), |acc, constraint| {
             format!("{}\n(assert {})", acc, constraint)
         });
-        let solver = Solver::new(&ctx.z3_context);
         let source_code = format!(
             "
 {}
@@ -317,18 +333,16 @@ fn check_rule(ctx: &AnalysysGlobalContext, rule: &Rule) -> Vec<AnalysysError> {
             limit_constraint
         );
         debug!("source code:\n{}", source_code);
-        solver.from_string(source_code);
-        match solver.check() {
-            SatResult::Sat => {
+        match solve(&source_code) {
+            SolverResult::Sat(example) => {
                 errors.push(AnalysysError::new(format!("over 1MB limit"), rule));
                 debug!("sat");
-                let model = solver.get_model().unwrap();
-                debug!("truthly example:\n{}", model);
+                debug!("truthly example:\n{}", example);
             }
-            SatResult::Unsat => {
+            SolverResult::Unsat => {
                 debug!("success unsat");
             }
-            SatResult::Unknown => errors.push(AnalysysError::new(
+            SolverResult::Unknown => errors.push(AnalysysError::new(
                 format!("Static analysis failed because this conditions are too complex."),
                 rule,
             )),
